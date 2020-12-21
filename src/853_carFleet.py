fe771baeed16dd1844f9a93cb67affcc2b3b14fc
zeroNeverFade <1261602439@qq.com>
2020-12-21 23:44:52 +0800

    add 1221

--- src/853_carFleet.py
+++ src/853_carFleet.py
@@ -1,21 +1,35 @@
 class Solution:
     def carFleet(self, target, position, speed):
-        return 0
+        if target == 0:
+            return 0
+        if not position or not speed:
+            return 0
+        cars = [[position[i], speed[i]] for i in range(len(position))]
+        cars.sort(key=lambda x: x[0], reverse=True)
+        n = len(position)
+        ret = n
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if meet(cars[i], cars[j]):
+                    ret -= 1
+        return ret
+
+
+def meet(a, b):
+    return True
 
 
 def main():
     # input
-    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    target = 12
+    position = [10, 8, 0, 5, 3]
+    speed = [2, 4, 1, 1, 3]
     # solution
     s = Solution()
-    ret = s.carFleet(nums)
+    ret = s.carFleet(target, position, speed)
     # out
     print("ret:", ret)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
